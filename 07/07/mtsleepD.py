3eaff8d8fcb077509aee53c06d75bfe35b29c73a
michael <iioelse@gmail.com>
2020-07-07 21:27:09 +0800

    practice

--- 07/07/mtsleepD.py
+++ 07/07/mtsleepD.py
@@ -5,4 +5,37 @@ from time import sleep,ctime
 
 loops = [4,2]
 
+class ThreadFunc:
+	def __init__(self, func, args, name=''):
+		self.name = name
+		self.func = func
+		self.args = args
 
+	def __call__(self):
+		self.func(*self.args)
+
+def loop(nloop,nsec):
+	print(f'start loop, {nloop}, at:, {ctime()}')
+	sleep(nsec)
+	print(f'loop, {nloop}, done at:, {ctime()}')
+
+
+def main():
+	print(f'starting at: {ctime()}')
+	threads = []
+	nloops = range(len(loops))
+
+	for i in nloops:
+		t = threading.Thread(target=ThreadFunc(loop,(i,loops[i]),loop.__name__))
+		threads.append(t)
+
+	for i in nloops:
+		threads[i].start()
+	
+	for i in nloops:
+		threads[i].join()
+	
+	print(f'all done at: {ctime}')
+
+if __name__ == '__main__':
+	main()
